f0059b229c49be9d9999514de6b95e4497c3d9b0
Scott Charlton <charlton@usgs.gov>
2019-05-17 17:52:45 -0600

    updated and simplified ctest.sh

--- ctest.sh
+++ ctest.sh
@@ -1,83 +1,54 @@
 #!/bin/env bash
 set -e
 
-#
-# gcc 7.1.0
-#
-VER=7.1.0
+for CMAKE_VER in 3.10.1
+do
+    # GCC
+    for GCC_VER in 6.1 7.1.0
+    do
+	# serial
+	module purge
+	module load cmake/${CMAKE_VER}
+	module load gcc/${GCC_VER}
+	ctest -S gcc.cmake -C Release -VV -O cmake-${CMAKE_VER}-gcc-${GCC_VER}.log -DBUILD_MPI:BOOL=OFF -DBUILD_OPENMP:BOOL=OFF -DCMAKE_VER:STRING=${CMAKE_VER} -DGCC_VER:STRING=${GCC_VER}
 
-# serial
-module purge
-module load cmake/3.9.0 gcc/${VER}
-rm -rf _gcc-${VER}
-ctest -S gcc.cmake -C Release -VV -O gcc-${VER}.log -DBUILD_MPI:BOOL=OFF -DBUILD_OPENMP:BOOL=OFF -DGCC_VER:STRING=${VER}
+	# openmp
+	module purge
+	module load cmake/${CMAKE_VER}
+	module load gcc/${GCC_VER}
+	ctest -S gcc.cmake -C Release -VV -O cmake-${CMAKE_VER}-gcc-${GCC_VER}-openmp.log -DBUILD_MPI:BOOL=OFF -DBUILD_OPENMP:BOOL=ON -DCMAKE_VER:STRING=${CMAKE_VER} -DGCC_VER:STRING=${GCC_VER}
+    done
 
-# openmp
-module purge
-module load cmake/3.9.0 gcc/${VER}
-rm -rf _gcc-${VER}_openmp
-ctest -S gcc.cmake -C Release -VV -O gcc-${VER}-openmp.log -DBUILD_MPI:BOOL=OFF -DBUILD_OPENMP:BOOL=ON -DGCC_VER:STRING=${VER}
+    # mpi (openmpi)
+    for OPENMPI in openmpi/1.10.2-gcc6.1.0 openmpi/2.1.2-gcc7.1.0  openmpi/3.0.0-gcc7.1.0 openmpi/1.10.2-gcc7.1.0 # openmpi/1.8.8-gcc6.1.0
+    do
+	module purge
+	module load cmake/${CMAKE_VER}
+	module load ${OPENMPI}
+	OPEN_MPI=$(echo ${OPENMPI} | sed -e s^/^_^g)
+	ctest -S gcc.cmake -C Release -VV -O cmake-${CMAKE_VER}-${OPEN_MPI}.log -DBUILD_MPI:BOOL=ON -DBUILD_OPENMP:BOOL=OFF -DCMAKE_VER:STRING=${CMAKE_VER} -DOPEN_MPI:STRING=${OPEN_MPI}
+    done
 
-# mpi
-module purge
-module load cmake/3.9.0 gcc/${VER} openmpi/3.0.0-gcc${VER}
-rm -rf _gcc-${VER}_mpi
-ctest -S gcc.cmake -C Release -VV -O gcc-${VER}-mpi.log -DBUILD_MPI:BOOL=ON -DBUILD_OPENMP:BOOL=OFF -DGCC_VER:STRING=${VER}
 
-#
-# gcc 6.1
-#
-VER=6.1
+    # INTEL
+    for INTEL_VER in 2015 2017u2 2018u1 2018u3
+    do
+	# serial
+	module purge
+	module load cmake/${CMAKE_VER}
+	module load intel/psxe-${INTEL_VER}
+	ctest -S intel.cmake -C Release -VV -O cmake-${CMAKE_VER}-intel_psxe-${INTEL_VER}.log -DBUILD_MPI:BOOL=OFF -DBUILD_OPENMP:BOOL=OFF -DCMAKE_VER:STRING=${CMAKE_VER} -DINTEL_VER:STRING=${INTEL_VER}
 
-# serial
-module purge
-module load cmake/3.9.0 gcc/${VER}
-rm -rf _gcc-${VER}
-ctest -S gcc.cmake -C Release -VV -O gcc-${VER}.log -DBUILD_MPI:BOOL=OFF -DBUILD_OPENMP:BOOL=OFF -DGCC_VER:STRING=${VER}
-
-# openmp
-module purge
-module load cmake/3.9.0 gcc/${VER}
-rm -rf _gcc-${VER}_openmp
-ctest -S gcc.cmake -C Release -VV -O gcc-${VER}-openmp.log -DBUILD_MPI:BOOL=OFF -DBUILD_OPENMP:BOOL=ON -DGCC_VER:STRING=${VER}
-
-# mpi
-# commit 8586c20 fails
-# w/o ZLIB_ROOT yields the following link error (vs2drt_mpi):
-# /cxfs/projects/spack/opt/spack/linux-scientific6-x86_64/gcc-6.1.0/libxml2-2.9.4-4beqwsumsysby4zmjngzjzgirfhlkek3/lib/libxml2.so.2: undefined reference to `gzopen64@ZLIB_1.2.3.3'
-module purge
-module load cmake/3.9.0 openmpi/1.10.2-gcc${VER}.0
-rm -rf _gcc-${VER}_mpi
-# export ZLIB_ROOT=/cxfs/projects/spack/opt/spack/linux-scientific6-x86_64/gcc-6.1.0/zlib-1.2.11-ybmzdn63hza4u56ny7uqbngjzj57gajo
-# using ZLIB_ROOT requires LD_LIBARY_PATH to be modified:
-# export LD_LIBARY_PATH=ZLIB_ROOT/lib:$LD_LIBRARY_PATH
-# see ~charlton/modulefiles/openmpi-src/1.10.2-gcc6.1.0
-ctest -S gcc.cmake -C Release -VV -O gcc-${VER}-mpi.log -DBUILD_MPI:BOOL=ON -DBUILD_OPENMP:BOOL=OFF -DGCC_VER:STRING=${VER}
-# unset ZLIB_ROOT
-
-#
-# intel_psxe-2015
-#
-
-# serial
-module purge
-module load cmake/3.9.0 intel/psxe-2015
-rm -rf _intel_psxe-2015
-ctest -S intel_psxe-2015.cmake -C Release -VV -O intel_psxe-2015.log -DBUILD_MPI:BOOL=OFF -DBUILD_OPENMP:BOOL=OFF
-
-# openmp
-module purge
-module load cmake/3.9.0 intel/psxe-2015
-rm -rf _intel_psxe-2015_openmp
-ctest -S intel_psxe-2015.cmake -C Release -VV -O intel_psxe-2015-openmp.log -DBUILD_MPI:BOOL=OFF -DBUILD_OPENMP:BOOL=ON
-
-# mpi
-module purge
-module load cmake/3.9.0 intel/psxe-2015
-rm -rf _intel_psxe-2015_mpi
-ctest -S intel_psxe-2015.cmake -C Release -VV -O intel_psxe-2015-mpi.log -DBUILD_MPI:BOOL=ON -DBUILD_OPENMP:BOOL=OFF
-
-#
-# unload all modules
-#
-module purge
+        # openmp
+	module purge
+	module load cmake/${CMAKE_VER}
+	module load intel/psxe-${INTEL_VER}
+	ctest -S intel.cmake -C Release -VV -O cmake-${CMAKE_VER}-intel_psxe-${INTEL_VER}.log -DBUILD_MPI:BOOL=OFF -DBUILD_OPENMP:BOOL=ON -DCMAKE_VER:STRING=${CMAKE_VER} -DINTEL_VER:STRING=${INTEL_VER}
+	
+        # mpi
+	module purge
+	module load cmake/${CMAKE_VER}
+	module load intel/psxe-${INTEL_VER}
+	ctest -S intel.cmake -C Release -VV -O cmake-${CMAKE_VER}-intel_psxe-${INTEL_VER}.log -DBUILD_MPI:BOOL=ON -DBUILD_OPENMP:BOOL=OFF -DCMAKE_VER:STRING=${CMAKE_VER} -DINTEL_VER:STRING=${INTEL_VER}
+    done
+done
